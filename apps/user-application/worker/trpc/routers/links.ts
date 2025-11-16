5042d88f1cb64c98970c7126a600694c273bc27e
Daniyal Ahmed <da9033431@gmail.com>
2025-11-16 21:52:40 +0000

    DB setup

--- apps/user-application/worker/trpc/routers/links.ts
+++ apps/user-application/worker/trpc/routers/links.ts
@@ -6,31 +6,38 @@ import {
 } from "@repo/data-ops/zod-schema/links";
 
 import { TRPCError } from "@trpc/server";
-import {
-  ACTIVE_LINKS_LAST_HOUR,
-  LAST_30_DAYS_BY_COUNTRY,
-  LINK_LIST,
-} from "./dummy-data";
+import { ACTIVE_LINKS_LAST_HOUR, LAST_30_DAYS_BY_COUNTRY } from "./dummy-data";
+import { createLink, getLinks, getLink } from "@repo/data-ops/queries/links";
 
 export const linksTrpcRoutes = t.router({
   linkList: t.procedure
     .input(
       z.object({
         offset: z.number().optional(),
-      }),
+      })
     )
-    .query(async ({}) => {
-      return LINK_LIST;
+    .query(async ({ ctx, input }) => {
+      const links = await getLinks(
+        ctx.userInfo.userId,
+        input.offset?.toString()
+      );
+      return links;
     }),
-  createLink: t.procedure.input(createLinkSchema).mutation(async ({}) => {
-    return "random-id";
-  }),
+  createLink: t.procedure
+    .input(createLinkSchema)
+    .mutation(async ({ ctx, input }) => {
+      const id = await createLink({
+        ...input,
+        accountId: ctx.userInfo.userId,
+      });
+      return id;
+    }),
   updateLinkName: t.procedure
     .input(
       z.object({
         linkId: z.string(),
         name: z.string().min(1).max(300),
-      }),
+      })
     )
     .mutation(async ({ input }) => {
       console.log(input.linkId, input.name);
@@ -39,21 +46,10 @@ export const linksTrpcRoutes = t.router({
     .input(
       z.object({
         linkId: z.string(),
-      }),
+      })
     )
-    .query(async ({}) => {
-      const data = {
-        name: "My Sample Link",
-        linkId: "link_123456789",
-        accountId: "user_987654321",
-        destinations: {
-          default: "https://example.com",
-          mobile: "https://mobile.example.com",
-          desktop: "https://desktop.example.com",
-        },
-        created: "2024-01-15T10:30:00Z",
-        updated: "2024-01-20T14:45:00Z",
-      };
+    .query(async ({ input }) => {
+      const data = await getLink(input.linkId);
       if (!data) throw new TRPCError({ code: "NOT_FOUND" });
       return data;
     }),
@@ -62,7 +58,7 @@ export const linksTrpcRoutes = t.router({
       z.object({
         linkId: z.string(),
         destinations: destinationsSchema,
-      }),
+      })
     )
     .mutation(async ({ input }) => {
       console.log(input.linkId, input.destinations);
