338e4007a5f23cfb731af39569e2ad6e9796750e
Daniyal Ahmed <da9033431@gmail.com>
2025-11-17 10:24:17 +0000

    Geo Based routing

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,4 +1,6 @@
+import { getDestinationForCountry } from '@/helpers/route-ops';
 import { getLink } from '@repo/data-ops/queries/links';
+import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
 import {Hono } from 'hono';
 
 export const app = new Hono<{Bindings:Env}>();
@@ -9,5 +11,11 @@ app.get('/:id', async (c) => {
     if (!linkInfo) {
         return c.text('Link not found', 404);
     }
-    return c.json({ linkInfo });
+    const cfHeaders = cloudflareInfoSchema.safeParse(c.req.raw.cf)
+    if (!cfHeaders.success) {
+        return c.text('Invalid Cloudflare headers', 400);
+    }
+    const headers = cfHeaders.data;
+    const destinations = getDestinationForCountry(linkInfo, headers.country);
+    return c.redirect(destinations);
 });
